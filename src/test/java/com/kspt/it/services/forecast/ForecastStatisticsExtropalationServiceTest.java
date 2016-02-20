a54e56662ba015f09ddf22f2b9f7645ff5cbe84d
johnkhandygo <johnkhandygo@gmail.com>
2016-02-21 01:50:54 +0300

    quick formatting

--- src/test/java/com/kspt/it/services/forecast/ForecastStatisticsExtropalationServiceTest.java
+++ src/test/java/com/kspt/it/services/forecast/ForecastStatisticsExtropalationServiceTest.java
@@ -1,36 +1,36 @@
 package com.kspt.it.services.forecast;
 
+import com.kspt.it.services.forecast.real.ForecastStatisticsExtropalationService;
 import javafx.util.Pair;
 import org.junit.Test;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class ForecastStatisticsExtropalationServiceTest {
 
-    @Test
-    public void testExtrapolateStatistics() throws Exception {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        Date date = dateFormat.parse("29-12-2015");
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
+  @Test
+  public void testExtrapolateStatistics()
+  throws Exception {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    Date date = dateFormat.parse("29-12-2015");
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
 
-        List<Pair<Float,Date>> inputList = new ArrayList<>();
+    List<Pair<Float, Date>> inputList = new ArrayList<>();
 
-        for(int i=0;i<6;i++) {
-            calendar.add(Calendar.DATE,1);
-            inputList.add(new Pair((float)i,calendar.getTime()));
-        }
-
-        List<Pair<Float,Date>> outputList = ForecastStatisticsExtropalationService.extrapolateStatistics(inputList,5);
-
-        for(Pair<Float,Date> pair : outputList) {
-            System.out.println(pair.getKey()+" "+pair.getValue());
-        }
+    for (int i = 0; i < 6; i++) {
+      calendar.add(Calendar.DATE, 1);
+      inputList.add(new Pair((float) i, calendar.getTime()));
     }
+
+    List<Pair<Float, Date>> outputList = ForecastStatisticsExtropalationService
+        .extrapolateStatistics(inputList, 5);
+
+    for (Pair<Float, Date> pair : outputList) {
+      System.out.println(pair.getKey() + " " + pair.getValue());
+    }
+  }
 }
