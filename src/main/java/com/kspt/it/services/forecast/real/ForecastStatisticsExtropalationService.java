936aeec0b7be73aab43fa7ebf1544de5f2852c8b
SkripalBoris <skipalboris@gmail.com>
2016-02-21 15:18:49 +0300

    Убраны предупреждения в классе экстрополяции

--- src/main/java/com/kspt/it/services/forecast/real/ForecastStatisticsExtropalationService.java
+++ src/main/java/com/kspt/it/services/forecast/real/ForecastStatisticsExtropalationService.java
@@ -6,69 +6,68 @@ import net.sourceforge.openforecast.DataSet;
 import net.sourceforge.openforecast.Forecaster;
 import net.sourceforge.openforecast.ForecastingModel;
 import net.sourceforge.openforecast.Observation;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 public class ForecastStatisticsExtropalationService {
-  /**
-   * Функция, экстрополирующая численный ряд
-   * @param inputList список пар Значение/Дата с данными за предыдущее время
-   * @param stepSize переменная, задающая на сколько шагов производится экстраполяция
-   * @return список пар Значение/Дата с предсказанным рядом
-   */
-  public static List<Pair<Float, Date>> extrapolateStatistics(
-      List<Pair<Float, Date>> inputList,
-      int stepSize) {
-    DataSet observedData = new DataSet();
+    /**
+     * Функция, экстрополирующая численный ряд
+     *
+     * @param inputList список пар Значение/Дата с данными за предыдущее время
+     * @param stepSize  переменная, задающая на сколько шагов производится экстраполяция. Если ее значние меньше
+     *                  единицы, то экстраполяция производится на 15 шагов вперед.
+     * @return список пар Значение/Дата с предсказанным рядом
+     */
+    public static List<Pair<Float, Date>> extrapolateStatistics(
+            final List<Pair<Float, Date>> inputList,
+            int stepSize) {
+        DataSet observedData = new DataSet();
+        Date maxDate = new Date(Long.MIN_VALUE);
+        final String tagString = "date";
 
-    Date maxDate = new Date(Long.MIN_VALUE);
+        for (Pair<Float, Date> pair : inputList) {
+            DataPoint dp = new Observation(pair.getKey());
+            dp.setIndependentValue(tagString, pair.getValue().getTime());
+            observedData.add(dp);
 
-    for (Pair<Float, Date> pair : inputList) {
-      DataPoint dp = new Observation(pair.getKey());
-      dp.setIndependentValue("date", pair.getValue().getTime());
-      observedData.add(dp);
+            if (maxDate.before(pair.getValue())) {
+                maxDate = pair.getValue();
+            }
+        }
 
-      if (maxDate.before(pair.getValue())) {
-        maxDate = pair.getValue();
-      }
+        observedData.sort(tagString);
+
+        ForecastingModel forecastingModel = Forecaster.getBestForecast(observedData);
+
+        if (stepSize < 1) {
+            stepSize = 15;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(maxDate);
+
+        DataSet requiredDataPoints = new DataSet();
+
+        for (int i = 0; i < stepSize; i++) {
+            calendar.add(Calendar.DATE, 1);
+            DataPoint dp = new Observation(0.0);
+            dp.setIndependentValue(tagString, calendar.getTime().getTime());
+            requiredDataPoints.add(dp);
+        }
+
+        forecastingModel.forecast(requiredDataPoints);
+
+        List<Pair<Float, Date>> returnList = new ArrayList<>();
+
+        for (DataPoint dp : requiredDataPoints) {
+            Double doubleTime = dp.getIndependentValue(tagString);
+            Date date = new Date(doubleTime.longValue());
+            Float val = (float) dp.getDependentValue();
+            returnList.add(new Pair<>(val, date));
+        }
+        return returnList;
     }
-
-    observedData.sort("date");
-
-    ForecastingModel forecastingModel = Forecaster.getBestForecast(observedData);
-
-    if (stepSize < 1) {
-      stepSize = 15;
-    }
-
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTime(maxDate);
-
-    DataSet requiredDataPoints = new DataSet();
-
-    for (int i = 0; i < stepSize; i++) {
-      calendar.add(Calendar.DATE, 1);
-      DataPoint dp = new Observation(0.0);
-      dp.setIndependentValue("date", calendar.getTime().getTime());
-      requiredDataPoints.add(dp);
-    }
-
-    forecastingModel.forecast(requiredDataPoints);
-
-    List<Pair<Float, Date>> returnList = new ArrayList<>();
-
-    Iterator<DataPoint> it = requiredDataPoints.iterator();
-
-    while (it.hasNext()) {
-      DataPoint dp = it.next();
-      Double doubleTime = dp.getIndependentValue("date");
-      Date date = new Date(doubleTime.longValue());
-      returnList.add(new Pair(dp.getDependentValue(), date));
-    }
-
-    return returnList;
-  }
 }
--- src/test/java/com/kspt/it/services/forecast/ForecastStatisticsExtropalationServiceTest.java
+++ src/test/java/com/kspt/it/services/forecast/ForecastStatisticsExtropalationServiceTest.java
@@ -3,34 +3,32 @@ package com.kspt.it.services.forecast;
 import com.kspt.it.services.forecast.real.ForecastStatisticsExtropalationService;
 import javafx.util.Pair;
 import org.junit.Test;
+
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class ForecastStatisticsExtropalationServiceTest {
 
-  @Test
-  public void testExtrapolateStatistics()
-  throws Exception {
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-    Date date = dateFormat.parse("29-12-2015");
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTime(date);
+    @Test
+    public void testExtrapolateStatistics()
+            throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = dateFormat.parse("29-12-2015");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
 
-    List<Pair<Float, Date>> inputList = new ArrayList<>();
+        List<Pair<Float, Date>> inputList = new ArrayList<>();
 
-    for (int i = 0; i < 6; i++) {
-      calendar.add(Calendar.DATE, 1);
-      inputList.add(new Pair((float) i, calendar.getTime()));
+        for (int i = 0; i < 6; i++) {
+            calendar.add(Calendar.DATE, 1);
+            inputList.add(new Pair<Float, Date>(new Float(i), calendar.getTime()));
+        }
+
+        List<Pair<Float, Date>> outputList = ForecastStatisticsExtropalationService
+                .extrapolateStatistics(inputList, 9);
+
+        for (Pair<Float, Date> pair : outputList) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
     }
-
-    List<Pair<Float, Date>> outputList = ForecastStatisticsExtropalationService
-        .extrapolateStatistics(inputList, 5);
-
-    for (Pair<Float, Date> pair : outputList) {
-      System.out.println(pair.getKey() + " " + pair.getValue());
-    }
-  }
 }
