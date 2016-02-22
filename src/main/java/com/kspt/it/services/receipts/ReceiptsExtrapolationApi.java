23e965d192c082da274d6ad8a59b0a3e8b3e83d5
SkripalBoris <skipalboris@gmail.com>
2016-02-22 13:14:08 +0300

    Добавлено представление для экстраполяции

---
+++ src/main/java/com/kspt/it/services/receipts/ReceiptsExtrapolationApi.java
@@ -0,0 +1,7 @@
+package com.kspt.it.services.receipts;
+
+import java.util.List;
+
+public interface ReceiptsExtrapolationApi {
+    List<ReceiptsExtrapolationAllShopsResult> extrapolateInAllShops();
+}
