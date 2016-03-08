2c8d45b3c8cc840639996c26cf839ed0bb66c2b9
johnkhandygo <johnkhandygo@gmail.com>
2016-03-08 12:30:22 +0300

    introduce new resource methods for accessing products info.

--- src/main/java/com/kspt/it/resources/MetaRetrievingResource.java
+++ src/main/java/com/kspt/it/resources/MetaRetrievingResource.java
@@ -1,15 +1,17 @@
 package com.kspt.it.resources;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Range;
 import com.kspt.it.services.meta.MetaRetrievingApi;
+import com.kspt.it.services.meta.Product;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toList;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -27,11 +29,7 @@ public class MetaRetrievingResource {
 
   @GET
   @Path("/dataCollectionDomain")
-  @ApiOperation(value = "Retrieves time domain of processed collection")
-  @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "OK"),
-      @ApiResponse(code = 500, message = "Something wrong in Server")
-  })
+  @ApiOperation(value = "Retrieves time domain of processed collection.")
   public DataCollectionDomainRepresentation getDataCollectionDomain() {
     final Range<Long> domain = api.getDataCollectionOrigin();
     return new DataCollectionDomainRepresentation(domain.lowerEndpoint(), domain.upperEndpoint());
@@ -39,16 +37,32 @@ public class MetaRetrievingResource {
 
   @GET
   @Path("/storesMeta")
-  @ApiOperation(value = "Retrieves stores meta")
-  @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "OK"),
-      @ApiResponse(code = 500, message = "Something wrong in Server")
-  })
-  public List<StoreMetaRepresentation> getStoresMeta() {
-    final List<StoreMetaRepresentation> list = api.getStoresMeta().stream()
-        .map(sm -> new StoreMetaRepresentation(sm.getId(), sm.getName()))
+  @ApiOperation(value = "Retrieves stores info.")
+  public List<StoreRepresentation> getStoresMeta() {
+    final List<StoreRepresentation> list = api.getStoresMeta().stream()
+        .map(sm -> new StoreRepresentation(sm.getId(), sm.getName()))
         .collect(toList());
     return list;
+  }
+
+  @GET
+  @Path("/products/all")
+  @ApiOperation(value = "Retrieves all products info.")
+  public List<ProductRepresentation> getAllProductsInfo() {
+    final List<ProductRepresentation> list = api.getAllProducts().stream()
+        .map(p -> new ProductRepresentation(p.getId(), p.getName()))
+        .collect(toList());
+    return list;
+  }
+
+  @GET
+  @Path("/products/{id}")
+  @ApiOperation(value = "Retrieves products info for a particular id.")
+  public ProductRepresentation getProductInfo(
+      final @PathParam("id") Integer id) {
+    final Product found = api.findProductInfo(id);
+    Preconditions.checkState(nonNull(found), "Product for id %s not found", id);
+    return new ProductRepresentation(found.getId(), found.getName());
   }
 }
 
@@ -70,15 +84,31 @@ class DataCollectionDomainRepresentation {
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-class StoreMetaRepresentation {
+class StoreRepresentation {
   private int id;
 
   private String name;
 
-  public StoreMetaRepresentation() {
+  public StoreRepresentation() {
   }
 
-  public StoreMetaRepresentation(final int id, final String name) {
+  public StoreRepresentation(final int id, final String name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+class ProductRepresentation {
+  private int id;
+
+  private String name;
+
+  public ProductRepresentation() {
+  }
+
+  public ProductRepresentation(final int id, final String name) {
     this.id = id;
     this.name = name;
   }
